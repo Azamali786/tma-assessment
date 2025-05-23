0a1dc0902b7e90ecfd27653f61f6b0d3129b3e16
azam <gausuleazamali@gmail.com>
2025-05-23 06:18:33 +0530

    added models,migrated and registed in admin

--- recipe_management/models.py
+++ recipe_management/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """
+    Represents a single ingredient that can be used in one or more recipes.
+
+    Attributes:
+        name (str): The name of the ingredient (e.g., "Salt", "Tomato").
+    """
+    name = models.CharField(max_length=100, help_text="Name of the ingredient")
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    """
+    Represents a recipe which contains a title and a set of ingredients.
+
+    Attributes:
+        title (str): The name or title of the recipe (e.g., "Spaghetti Bolognese").
+        ingredients (ManyToMany): A many-to-many relationship with Ingredient.
+    """
+    title = models.CharField(max_length=100, help_text="Title of the recipe")
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        related_name='recipes',
+        help_text="List of ingredients used in the recipe"
+    )
+
+    def __str__(self):
+        return self.title
